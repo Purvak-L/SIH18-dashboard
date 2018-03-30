49c3b4f466b3e9807f7e2f68d8d72a398c0d6f2e
ashok05@somaiya.edu <ashok05@somaiya.edu>
2018-03-30 20:25:11 +0530

    commit v0.4

--- src/js/jcrop_main.js
+++ src/js/jcrop_main.js
@@ -17,10 +17,19 @@ function updateCoords(c) {
     var rx = 200 / c.w; // 200 - preview box size
     var ry = 200 / c.h;
     document.getElementById("latest_coordinates").innerHTML = "<br>Located Co-ordinates: "+c.x+":"+c.y+":"+c.x2+":"+c.y2;
-    localStorage.setItem("x1",c.x);
-    localStorage.setItem("y1",c.y);
-    localStorage.setItem("x2",c.x2);
-    localStorage.setItem("y2",c.y2);
+    // localStorage.setItem("x1",c.x);
+    // localStorage.setItem("y1",c.y);
+    // localStorage.setItem("x2",c.x2);
+    // localStorage.setItem("y2",c.y2);
+    sx = (c.x)*(1920/800);
+    sy = (c.y)*(1080/500);
+    sx2 = (c.x2)*(1920/800);
+    sy2 = (c.y2)*(1080/500);
+    console.log("Scaled Coordinates: "+sx+":"+sy+":"+sx2+":"+sy2);
+    localStorage.setItem("x1",sx);
+    localStorage.setItem("y1",sy);
+    localStorage.setItem("x2",sx2);
+    localStorage.setItem("y2",sy2);
 };
 
 function checkCoords() {
--- src/reader.js
+++ src/reader.js
@@ -26,8 +26,11 @@ var drone_loc = {
 
 function check_mission_status() {
 	if (paramsobj.x1 == 0 || paramsobj.y1 == 0 || paramsobj.x2 == 0 || paramsobj.y2 == 0 || paramsobj.height == 0 || paramsobj.overlap == 0 || paramsobj.no_drones == 0 || paramsobj.comm_range == 0 || paramsobj.time_of_flight == 0) {
+		console.log('entered check mission status');
 		document.getElementById('mission_button').innerHTML = '<button type="button" class="btn cur-p btn-danger"></button>';
 		document.getElementById('mission_ready_state').innerHTML ='<h6 class="lh-1">Status: <span style="color: #ff3f80">NOT READY</span></h6>';
+		document.getElementById('drone_tip').innerHTML = '<div class="alert alert-danger" role="alert">Please submit all params to get estimates on <strong>time</strong> and <strong>number of drones</strong></div>';
+		
 	}
 }
 
@@ -41,6 +44,7 @@ function start_mission() {
 function send_params() {
 
 	console.log('entered send_params');
+	document.getElementById('drone_tip').innerHTML = '<div class="alert alert-success" role="alert"><strong>Estimation:</strong> The selected area will be mapped by <strong>4</strong> drones in <strong>20 minutes</strong></div>';
 	paramsobj.height = document.getElementById("height").value;
 	paramsobj.overlap = document.getElementById("overlap").value;
 	paramsobj.no_drones = document.getElementById("no_drones").value;
@@ -73,20 +77,27 @@ function set_level(number) {
 
 function doLoad() {
 	// console.log(allcookies);
-	check_mission_status();
+	// check_mission_status();
 	if (location.href.split("/").slice(-1) == "index.html") {
-			disp = document.getElementById("main-image");
-    		dispCtx = disp.getContext("2d");
-    		im = new Image();
-    		im.onload = function() {
-    		disp.setAttribute("width", im.width);
-    		disp.setAttribute("height", im.height);
-    		dispCtx.drawImage(this, 0, 0);
-  		};	
+			check_mission_status();
+
+			// disp = document.getElementById("main-image");
+   //  		dispCtx = disp.getContext("2d");
+   //  		im = new Image();
+   //  		im.onload = function() {
+   //  		disp.setAttribute("width", im.width);
+   //  		disp.setAttribute("height", im.height);
+   //  		dispCtx.drawImage(this, 0, 0);
+   //  		base_image.src = 'main-curr-view.png';
+   //  		base_image.onload = function(){
+   // 			dispCtx.drawImage(base_image, 0, 0);
+
+  			// }
+  		// };	
 	}
     
-    // ws = new WebSocket("ws://127.0.0.1:50011");
-    ws = new WebSocket("ws://172.20.10.2:8080");
+    ws = new WebSocket("ws://172.20.10.2:50050");
+    // ws = new WebSocket("ws://172.20.10.2:8080");
    
     ws.onmessage = function (evt) {
     	console.log(evt.data)
@@ -97,6 +108,7 @@ function doLoad() {
     	if (data.type == "drones") {
     		console.log(data);
     		if (location.href.split("/").slice(-1) == "index.html") {
+
     			setDroneTableData(data);
     			set_drone_data_flag = 1;
     		}
@@ -106,7 +118,10 @@ function doLoad() {
     		// console.log('img data received');
     		console.log(data);
     		if (location.href.split("/").slice(-1) == "index.html") {
-    			im.src = "data:image/png;base64," + data.img_data;
+    			var img_data = dispCtx.createImageData(1366,768);
+    			img_data.data = data.data;
+    			dispCtx.putImageData(img_data,0,0);
+    			// im.src = "data:image/png;base64," + data.img_data;
     		}
     	}
     	// to handle relay points data
@@ -118,6 +133,32 @@ function doLoad() {
     	else if (data.type == "grid_data") {
     		setGridTableData(data);
     		console.log(data);
+    		
+    		if (location.href.split("/").slice(-1) == "index.html") {
+    		var total_time = 0;
+    		var number_of_grids = data.blocks.length;
+    		count_of_explored = 0;
+    		for (var i=0; i<number_of_grids; i++) {
+    			// console.log(data.blocks[i].est_time);
+    			total_time += parseFloat(data.blocks[i].est_time)
+    			if (data.blocks[i].status == 'explored' ) {
+    				count_of_explored += 1;
+    			}
+    		}
+    		console.log(total_time);
+    		average = total_time/number_of_grids;
+    		percent_complete = (count_of_explored/number_of_grids)*100;
+    		console.log(Math.floor(percent_complete.toString()));
+    		// console.log(document.getElementById('grid_exploration_percent'));
+    		// console.log(typeof(percent_complete.toString()));
+    		// class="easy-pie-chart";
+    		// aria-valuenow="50"
+    		// document.getElementById('grid_exploration_percent').setAttribute("data-percent","50"); 
+    		// document.getElementById('grid_progress').setAttribute("aria-valuenow","50"); 
+    		document.getElementById("grid_progress").innerHTML = "Grid exploration: "+percent_complete.toString()+"%";
+    		document.getElementById("est_time").innerHTML = "ETA: "+average;
+    		// document.getElementById('grid_exploration_div').innerHTML = '<div id="grid_exploration_percent" class="easy-pie-chart" data-size='+percent_complete.toString()+'data-bar-color=\'#f44336\'><span></span></div><h6 class="fsz-sm">New Users</h6>'
+    	}
     	}
 
     	else if (data.type == "drone_conn") {
@@ -128,6 +169,8 @@ function doLoad() {
     	else if (data.type == "ready") {
     		document.getElementById('mission_button').innerHTML = '<button type="button" class="btn cur-p btn-success" onclick="start_mission()">Start mission</button>';
 			document.getElementById('mission_ready_state').innerHTML ='<h6 class="lh-1">Status: <span style="color: #008975">READY</span></h6>';
+			// TODO: Get estimated number of drones and time from server
+			document.getElementById('drone_tip').innerHTML = '<div class="alert alert-success" role="alert"><strong>Estimation:</strong> The selected area will be mapped by <strong>4</strong> drones in <strong>20 minutes</strong></div>';
     	}   
     }
     if (document.getElementById('weather_tip')){
